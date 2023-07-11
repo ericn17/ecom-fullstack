53924aeb00f3ebf077d89f7fbf289a97e80dba26
Eric Nguyen <15enguyen4@gmail.com>
2023-07-11 14:49:30 -0700

    setup

---
+++ app/components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+      Hello World 2.0!
+    </div>
+  );
+}
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Navbar from './components/navbar'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+        Hello World!
+        <Navbar />  
     </main>
   )
 }
